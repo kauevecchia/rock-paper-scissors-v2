58ad84a798d8ae8a0dbed7986d9db5223aabb132
Kauê <kauevecchia@hotmail.com>
2025-02-11 22:30:43 -0300

    feat: add modal component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header";
 import { Play } from "./components/Play";
 import { Results } from "./components/Results";
 import { Footer } from "./components/Footer";
+import { Modal } from "./components/Modal";
 
 export function App() {
     return (
@@ -9,6 +10,7 @@ export function App() {
             <Header />
             <Play />
             <Results />
+            <Modal />
             <Footer />
         </>
     )
---
+++ src/components/Modal.jsx
@@ -0,0 +1,17 @@
+export function Modal() {
+    return (
+        <div id="rulesModal" className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center hidden">
+            <div className="bg-white p-6 rounded-lg max-w-md w-full z-10">
+                <h2 className="text-xl font-bold mb-4">Game rules</h2>
+                <ul className="mb-4">
+                    <li>Rock wins against scissors.</li>
+                    <li>Paper wins against rock.</li>
+                    <li>Scissors wins against paper.</li>
+                    <li>If both players select the same option, it's a draw.</li>
+                    <li className="font-bold text-red-600">**You can't select rock twice in a row.**</li>
+                </ul>
+                <button id="closeBtn" className="w-full px-4 py-2 bg-sky-600 hover:bg-sky-700 transition duration-200 text-white rounded-md">Close</button>
+            </div>
+        </div>
+    )
+}
