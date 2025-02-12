6ebad87edf0021048ba7261b56633b359e5c8743
Kauê <kauevecchia@hotmail.com>
2025-02-11 22:30:10 -0300

    feat: add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Header } from "./components/Header";
 import { Play } from "./components/Play";
 import { Results } from "./components/Results";
+import { Footer } from "./components/Footer";
 
 export function App() {
     return (
         <>
-            <Header></Header>
-            <Play></Play>
-            <Results></Results>
+            <Header />
+            <Play />
+            <Results />
+            <Footer />
         </>
     )
 }
