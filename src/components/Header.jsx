30fc914d291aefb799db8d015537bd41c3b583e0
Kauê <kauevecchia@hotmail.com>
2025-02-14 02:25:23 -0300

    feat: receive resetScoreboard function as prop and bind to reset button onClick

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-export function Header() {
+export function Header({ gameState, resetScoreboard }) {
     return (
         <header className="min-h-28 bg-sky-700 w-full flex items-center justify-between p-8">
             <div className="max-w-7xl w-full mx-auto flex items-center justify-between">
@@ -6,10 +6,10 @@ export function Header() {
                 <div className="bg-white flex gap-4 font-bold flex-col shadow-md rounded-md p-4">
                     <div>
                         <h2 className="text-base md:text-lg">Scoreboard:</h2>
-                        <p className="text-sm md:text-base">Player: <span id="playerScore">0</span></p>
-                        <p className="text-sm md:text-base">Machine: <span id="machineScore">0</span> </p>
+                        <p className="text-sm md:text-base">Player: <span id="playerScore">{gameState.playerScore}</span></p>
+                        <p className="text-sm md:text-base">Machine: <span id="machineScore">{gameState.machineScore}</span> </p>
                     </div>
-                    <button id="resetBtn" className="px-4 py-2 bg-sky-700 rounded-md text-white hover:bg-sky-900 transition duration-200">Reset</button>
+                    <button id="resetBtn" className="px-4 py-2 bg-sky-700 rounded-md text-white hover:bg-sky-900 transition duration-200" onClick={resetScoreboard}>Reset</button>
                 </div>
             </div>
         </header>
