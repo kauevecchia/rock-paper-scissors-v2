b1202a06c2efa2df263db9dfcd9066b527b023c7
Kauê <kauevecchia@hotmail.com>
2025-02-14 02:17:14 -0300

    feat: pass handleChoice function to onClick of each button for player selection

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -11,13 +11,13 @@ export function Play({ handlePlayGame }) {
         <div id="options-container" className="flex flex-col items-center justify-center animate-showUp">
             <h2 className="text-sky-700 font-bold text-2xl mb-4 md:text-3xl">Choose one: </h2>
             <div id="options" className="flex gap-4 md:gap-6 flex-row items-center">
-                <button className="choice-button w-24 h-24 md:w-28 md:h-28 hover:scale-110 transition duration-300 ease-in-out" data-choice="rock" onClick={handlePlayGame}>
+                <button className="choice-button w-24 h-24 md:w-28 md:h-28 hover:scale-110 transition duration-300 ease-in-out" data-choice="rock" onClick={() => handleChoice("rock")}>
                     <img src={rockImg} alt="rock" />
                 </button>
-                <button className="choice-button w-24 h-24 md:w-28 md:h-28 hover:scale-110 transition duration-300 ease-in-out" data-choice="paper" onClick={handlePlayGame}>
+                <button className="choice-button w-24 h-24 md:w-28 md:h-28 hover:scale-110 transition duration-300 ease-in-out" data-choice="paper" onClick={() => handleChoice("paper")}>
                     <img src={paperImg} alt="paper" />
                 </button>
-                <button className="choice-button w-24 h-24 md:w-28 md:h-28 hover:scale-110 transition duration-300 ease-in-out" data-choice="scissors" onClick={handlePlayGame}>
+                <button className="choice-button w-24 h-24 md:w-28 md:h-28 hover:scale-110 transition duration-300 ease-in-out" data-choice="scissors" onClick={() => handleChoice("scissors")}>
                     <img src={scissorsImg} alt="scissors" />
                 </button>
             </div>
